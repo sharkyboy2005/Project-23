af174be8bac57b9196c5affa8d7d9bc5dcfcfd6a
sharkyboy2005 <70273799+sharkyboy2005@users.noreply.github.com>
2020-10-31 11:01:51 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
-var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
-var packageBody,ground
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+var helicopterIMG, helicopterSprite, packageSprite,packageIMG;
+var packageBody,ground;
+var dropZone,lefty,righty;
+var dropZoneBody, leftyBody, rightyBody;
 
 function preload()
 {
@@ -15,6 +17,8 @@ function setup() {
 	createCanvas(800, 700);
 	rectMode(CENTER);
 	
+	engine = Engine.create();
+	world = engine.world;
 
 	packageSprite=createSprite(width/2, 80, 10,10);
 	packageSprite.addImage(packageIMG)
@@ -27,11 +31,23 @@ function setup() {
 	groundSprite=createSprite(width/2, height-35, width,10);
 	groundSprite.shapeColor=color(255)
 
+	
+	dropZone=createSprite(width/2, 650, 200, 20)
+	dropZoneBody = Bodies.rectangle(width/2 , 200 , 5 , {isStatic:true});
+	dropZone.shapeColor = 'red'
+	World.add(world, dropZone);
 
-	engine = Engine.create();
-	world = engine.world;
+	lefty=createSprite(width/2-100, 600, 20, 100)
+	leftyBody = Bodies.rectangle(width/2 , 200 , 5 , {isStatic:true});
+	lefty.shapeColor = 'red'
+	World.add(world, lefty);
 
-	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:3, isStatic:true});
+	righty=createSprite(width/2 + 100, 600, 20, 100)
+	rightyBody = Bodies.rectangle(width/2 , 200 , 5 , {isStatic:true});
+	righty.shapeColor = 'red'
+	World.add(world, righty);
+
+	packageBody = Bodies.circle(width/2 , 200 , 5 , {restitution:0.5, isStatic:true});
 	World.add(world, packageBody);
 	
 
@@ -46,20 +62,17 @@ function setup() {
 
 
 function draw() {
-  rectMode(CENTER);
-  background(0);
-  packageSprite.x= packageBody.position.x 
-  packageSprite.y= packageBody.position.y 
-  drawSprites();
- 
+  	rectMode(CENTER);
+	background(0);
+  	packageSprite.x= packageBody.position.x 
+  	packageSprite.y= packageBody.position.y 
+	drawSprites();
 }
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
+	// Look at the hints in the document and understand how to make the package body fall only on
+	Matter.Body.setStatic(packageBody,false);
     packageSprite.velocityY = 5
   }
 }
-
-
-
